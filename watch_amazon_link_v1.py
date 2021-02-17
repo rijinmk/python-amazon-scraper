7827f458a5b74a4ca8c06b84e54843674b1e08c0
Rijin Mk <rijin.mk9@gmail.com>
2021-02-17 19:13:05 +0400

    HTML Page

--- watch_amazon_link_v1.py
+++ watch_amazon_link_v1.py
@@ -10,7 +10,6 @@ Todo:
 '''
 
 import requests
-import urllib2
 import time
 import bs4
 from datetime import datetime
@@ -29,11 +28,22 @@ def playASound():
     elif sys.platform == "darwin":
         os.system('say Oh my god buy now')
 
+headers = {
+'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36',
+}
+
 while True: 
-    page = requests.get(url)
+    page = requests.get(url, headers=headers)
+    print(page)
     html = page.text
-    soup = bs4.BeautifulSoup(html, 'html.parser')
-    
+    soup = bs4.BeautifulSoup(html, 'html.parser').encode('utf-8')
+
+    #testing
+    f = open('ps5.html', 'wb')
+    f.write(soup)
+    f.close()
+    break
+
     # Extract information
     price = float(soup.find("span", {"id" : "priceblock_ourprice"}).text.replace(u'\xa0', u' ').split(' ')[1].replace(u',', u''))
     stock = soup.find("div", {"id" : "availability"}).text.strip()
